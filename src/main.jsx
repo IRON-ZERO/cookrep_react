4cedcf265d4ef0ffcac0c2ff5c037a67f49abce2
Hellicat <jhccop@naver.com>
2025-11-06 15:49:10 +0900

    📝 [Chore] initialized templates
    [Chore] initialized templates

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
-import "./index.css";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {RouterProvider} from "react-router";
 import {router} from "./router.jsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
