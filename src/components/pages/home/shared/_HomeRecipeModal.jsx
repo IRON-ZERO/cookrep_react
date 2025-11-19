a3dcd27188fb01a7b27d97972c5c60337745acab
Hellicat <jhccop@naver.com>
2025-11-19 15:12:44 +0900

    📝 [Feat] MainPage/Create Modal/Edit Header Footer
    [Feat] MainPage/Create Modal/Edit Header Footer

---
+++ src/components/pages/home/shared/_HomeRecipeModal.jsx
@@ -0,0 +1,58 @@
+import NutritionFacts from "./recipeModalComp/_NutritionFacts";
+
+export default function HomeRecipeModal({recipe, setModal}) {
+  const mapMannual = Array.from({length: 20}, (v, i) => {
+    const index = String(i + 1).padStart(2, "0");
+    const manual = recipe[`MANUAL${index}`];
+    const manualImg = recipe[`MANUAL_IMG${index}`];
+    if (
+      (manual && manual.trim !== "") ||
+      (manualImg && manualImg.trim !== "")
+    ) {
+      return {
+        index,
+        manual,
+        manualImg,
+      };
+    }
+  }).filter((i) => i !== undefined);
+  console.log(mapMannual);
+  return (
+    <article className="modal-animate fixed z-50 top-0 left-0 bg-slate-900/50 w-full h-full flex justify-center items-center">
+      <div onClick={() => setModal("")} className="absolute w-full h-full" />
+      <div className="bg-(--ck-white) rounded-md flex flex-col gap-3 relative overflow-auto h-[90%]">
+        <div className="relative left-0 top-0">
+          <img
+            src={recipe.ATT_FILE_NO_MK}
+            alt={recipe.RCP_NM}
+            className="w-full h-[600px] brightness-80"
+          />
+        </div>
+        <div className="flex flex-col gap-5 p-6">
+          <h3 className="text-4xl text-(--ck-orange)">{recipe.RCP_NM}</h3>
+          <NutritionFacts
+            car={recipe.INFO_CAR}
+            eng={recipe.INFO_ENG}
+            fat={recipe.INFO_FAT}
+            na={recipe.INFO_NA}
+            pro={recipe.INFO_PRO}
+          />
+          <ul>
+            {mapMannual?.map((item) => (
+              <li>
+                <img src={item.manualImg} alt={item.manual} loading="lazy" />
+                <p>{item.manual}</p>
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={() => setModal("")}
+            className="bg-(--ck-orange) p-2 rounded-md"
+          >
+            닫기
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+}
---
+++ src/components/pages/home/shared/recipeModalComp/_NutritionFacts.jsx
@@ -0,0 +1,29 @@
+export default function NutritionFacts({car, eng, fat, na, pro}) {
+  return (
+    <aside className="flex flex-col gap-3">
+      <h4 className="text-2xl font-semibold">영양성분표</h4>
+      <div className="grid grid-cols-2 *:flex *:gap-3">
+        <p>
+          <span>탄수화물</span>
+          <span>{car} g</span>
+        </p>
+        <p>
+          <span>열량</span>
+          <span>{eng} KCAL</span>
+        </p>
+        <p>
+          <span>지방</span>
+          <span>{fat}g</span>
+        </p>
+        <p>
+          <span>나트륨</span>
+          <span>{na} mg</span>
+        </p>
+        <p>
+          <span>프로틴</span>
+          <span>{pro} g</span>
+        </p>
+      </div>
+    </aside>
+  );
+}
